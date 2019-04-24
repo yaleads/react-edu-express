6f1a0f35ff42035c60b6493118ec0a7e36840347
artemiarmilko <yarm@mail.ru>
2019-04-23 23:04:48 -0700

    react hello world

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import math_info, {sum as math_sum, dif as math_dif, mult as math_mult, div as math_div} from '~/src/Math'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
 math_info();
 console.log(`3 + 4 = ${math_sum(3, 4)}`);
 console.log(`5 - 4 = ${math_dif(5, 4)}`);
 console.log(`5 * 4 = ${math_mult(5, 4)}`);
 console.log(`90 / 5 = ${math_div(90, 5)}`);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Hello World!</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
