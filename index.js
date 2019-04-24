cce457230d9813a14c3fe9955079a57c6eb58633
artemiarmilko <yarm@mail.ru>
2019-04-23 22:55:56 -0700

    simple math

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+import math_info, {sum as math_sum, dif as math_dif, mult as math_mult, div as math_div} from '~/src/Math'
+
+math_info();
+console.log(`3 + 4 = ${math_sum(3, 4)}`);
+console.log(`5 - 4 = ${math_dif(5, 4)}`);
+console.log(`5 * 4 = ${math_mult(5, 4)}`);
+console.log(`90 / 5 = ${math_div(90, 5)}`);
---
+++ src/Math.js
@@ -0,0 +1,8 @@
+export const sum = (a, b) => (a + b);
+export const dif = (a, b) => (a - b);
+export const mult = (a, b) => (a * b);
+export const div = (a, b) => (a / b);
+
+const printInfo = () => (console.log('Available options: + - * /'));
+
+export default printInfo;
