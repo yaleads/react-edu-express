826890132367100bc9f30a46075f236edcc746a6
artemiarmilko <yarm@mail.ru>
2019-04-24 12:11:34 -0700

    Math

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import Math from '~/src/components/Math'
+import math_info, {sum as math_sum, dif as math_dif, mult as math_mult, div as math_div} from '~/src/Math'
+
+math_info();
+console.log(`3 + 4 = ${math_sum(3, 4)}`);
+console.log(`5 - 4 = ${math_dif(5, 4)}`);
+console.log(`5 * 4 = ${math_mult(5, 4)}`);
+console.log(`90 / 5 = ${math_div(90, 5)}`);
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +17,6 @@ class App extends Component {
     return (
       <div>
         <h1>Hello World!</h1>
-        <Math />
       </div>
     );
   }
