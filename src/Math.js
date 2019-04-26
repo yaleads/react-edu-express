f5396ac461421707788e30caa245d772a8bfb69e
artemiarmilko <yarm@mail.ru>
2019-04-26 14:24:05 -0700

    Products

--- src/Math.js
+++
@@ -1,8 +0,0 @@
-export const sum = (a, b) => (a + b);
-export const dif = (a, b) => (a - b);
-export const mult = (a, b) => (a * b);
-export const div = (a, b) => (a / b);
-
-const printInfo = () => ('Available options: + - * /');
-
-export default printInfo;
