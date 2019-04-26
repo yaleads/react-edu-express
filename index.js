0d80c0801d1715c5b2a2cd42e429f6f5ed8ede4d
artemiarmilko <yarm@mail.ru>
2019-04-26 15:16:54 -0700

    CatalogPage

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
-import Products from '~/src/constants/Products'
-import ProductCatalog from "./src/components/ProductCatalog";
+
+import CatalogPage from "./src/components/CatalogPage";
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <div className='container'>
         <h1>React App</h1>
-        <ProductCatalog products={ Products() } />
+        <CatalogPage />
       </div>
     );
   }
